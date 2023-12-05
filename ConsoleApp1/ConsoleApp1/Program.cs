f6dd270d301b8638c3f687123c5164468a0b36e5
Август <@LAPTOP-63G20V4O>
2023-12-05 21:41:14 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿# include <windows.h>    // Подключаем описания функций API, стандартных типов
+
+int WINAPI WinMain (HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return MessageBox(NULL, LPCWSTR("Hello, Windows!"), LPCWSTR("First Windows Program"), MB_OK | MB_ICONEXCLAMATION);
+}
---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,7 @@
+﻿
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return MessageBox(NULL, LPCWSTR("Hello, Windows!"), LPCWSTR("First Windows Program"), MB_OK | MB_ICONEXCLAMATION);
+}
